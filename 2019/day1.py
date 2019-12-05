1896a6321902e368a644fba9bbbc8caece5f7b87
Josh Bright <josh.bright@gmail.com>
2019-12-04 22:09:16 -0800

    Cleaned up file and added some comments

--- 2019/day1.py
+++ 2019/day1.py
@@ -7,26 +7,41 @@ def calculate_fuel_usage(amount):
     return val - 2
 
 
-def main():
+def part1():
+    """
+    For the first part, we need to read in data from a text file
+    each line is the amount of mass for something
+    we need to calculate how much fuel that will take
+    and then return the total
+    """
     filepath = os.path.join(DATA_DIR, "day1-input.txt")
     lines = get_file_lines(filepath)
     total = 0
     for line in lines:
         total += calculate_fuel_usage(int(line))
-    print(total)
+    return total
 
 
-def main2():
+def part2():
+    """
+    For part 2 we need to the same thing, but,
+    we need to also calculate fuel for the fuel we are adding
+    """
     filepath = os.path.join(DATA_DIR, "day1-input.txt")
     lines = get_file_lines(filepath)
     total = 0
     for line in lines:
         this_total = calculate_fuel_usage(int(line))
         while (this_total > 0):
+            # each time we loop, the amount of fuel will decrease
+            # until we hit zero, once that happens we're done
             total += this_total
             this_total = calculate_fuel_usage(this_total)
-    print(total)
+    return total
 
+def main():
+    print("Answer for Part 1: {}".format(part1()))
+    print("Answer for Part 2: {}".format(part2()))
 
 if __name__ == "__main__":
-    main2()
+    main()
