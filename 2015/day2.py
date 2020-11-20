2ecc72b7b6b2282bf0a1cde2b0fe6ddb80cdfdff
Josh Bright <josh.bright@gmail.com>
2020-11-19 20:12:22 -0800

    2015 - Added Day 2 Part 2 solution

--- 2015/day2.py
+++ 2015/day2.py
@@ -1,6 +1,15 @@
 import common
 
 
+def get_ribbon_length(width, length, height):
+	vals = [width, length, height]
+	max_val = max(vals)
+	vals.remove(max_val)
+	result = vals[0] * 2 + vals[1] * 2
+	result += width * length * height
+	return result
+
+
 def get_area(width, length, height):
 	side1 = 2 * length * width
 	side2 = 2 * width * height
@@ -11,7 +20,7 @@ def get_area(width, length, height):
 	return side1 + side2 + side3 + small_area
 
 
-def main():
+def part1():
 	data = common.get_file_contents("data/day2_input.txt")
 
 	result = 0
@@ -19,6 +28,20 @@ def main():
 		parts = line.split("x")
 		result += get_area(int(parts[0]), int(parts[1]), int(parts[2]))
 	print(result)
+
+
+def part2():
+	data = common.get_file_contents("data/day2_input.txt")
+	result = 0
+
+	for line in data:
+		parts = line.split("x")
+		result += get_ribbon_length(int(parts[0]), int(parts[1]), int(parts[2]))
+	print(result)
+
+
+def main():
+	part2()
 
 if __name__ == '__main__':
 	main()
