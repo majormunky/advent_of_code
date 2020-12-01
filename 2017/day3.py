12a71e9183a30d4c72c1b2879410c244da9a9b8a
Josh Bright <jbright@hagadonemedia.com>
2020-12-01 11:42:17 -0800

    2017 - Day 3 Part 1 Started

---
+++ 2017/day3.py
@@ -0,0 +1,27 @@
+import sys
+import common
+
+
+data = "347991"
+
+
+def part1():
+    print(len(data))
+    return "not complete"
+
+
+def part2():
+    return "not complete"
+
+
+def main():
+    part1_answer = part1()
+    part2_answer = part2()
+
+    print(f"Part 1: {part1_answer}")
+    print(f"Part 2: {part2_answer}")
+
+
+if __name__ == '__main__':
+    main()
+
