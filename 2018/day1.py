375a9689536a26a30a7a30682068257f5c6b78bd
Josh Bright <jbright@hagadonemedia.com>
2020-12-03 22:16:33 -0800

    2018 Day 1 Part 1 Solution Added

---
+++ 2018/day1.py
@@ -0,0 +1,38 @@
+import sys
+import common
+
+
+def get_filename():
+    filename = sys.argv[0]
+    filename = filename.split("/")[-1]
+    filename = filename.split(".")[0]
+    return filename
+
+data = common.get_file_contents("data/{}_input.txt".format(get_filename()))
+
+
+def part1():
+    answer = 0
+    for row in data:
+        if "-" in row:
+            answer -= int(row[1:])
+        else:
+            answer += int(row[1:])
+    return answer
+
+
+def part2():
+    return "not complete"
+
+
+def main():
+    part1_answer = part1()
+    part2_answer = part2()
+
+    print(f"Part 1: {part1_answer}")
+    print(f"Part 2: {part2_answer}")
+
+
+if __name__ == '__main__':
+    main()
+
