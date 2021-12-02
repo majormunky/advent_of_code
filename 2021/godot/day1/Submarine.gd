4da839467edd542d0943ef093f79339d58b90a80
Josh Bright <josh.bright@gmail.com>
2021-12-01 17:22:55 -0800

    We now move the submarine to show more of the sea floor

--- 2021/godot/day1/Submarine.gd
+++ 2021/godot/day1/Submarine.gd
@@ -1,14 +1,16 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var velocity = Vector2.ZERO
+const SPEED = 40
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	velocity = Vector2(SPEED, 0)
+
+
+func _physics_process(delta):
+	velocity = move_and_slide(velocity)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
