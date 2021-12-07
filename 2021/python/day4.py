e35102b5112435692561ae49873b816a6f559a1b
Josh Bright <josh.bright@gmail.com>
2021-12-06 20:11:53 -0800

    2021 Day 4 Part 1 solution added

---
+++ 2021/python/day4.py
@@ -0,0 +1,113 @@
+from common import get_file_contents
+
+
+class BingoBoard:
+	def __init__(self, data):
+		self.lines = []
+		for line in data:
+			
+			parts = line.split(" ")
+			new_line = [x for x in parts if x != ""]
+
+			self.lines.append(new_line)
+		self.marked = [[False for _ in range(5)] for _ in range(5)]
+
+	def check_number(self, num):
+		for index, line in enumerate(self.lines):
+			if num in list(line):
+				num_index = line.index(num)
+				try:
+					self.marked[index][num_index] = True
+				except IndexError:
+					print("Bad index!", index, num_index)
+
+	def check_board(self):
+		for line in self.marked:
+			if all(line):
+				return True
+
+		index_check = True
+		for i in range(5):
+			for line in self.marked:
+				if not line[i]:
+					index_check = False
+
+			if index_check:
+				return True
+		return index_check
+
+	def get_score(self):
+		score = 0
+		for line_index, line in enumerate(self.marked):
+			for col_index, col in enumerate(line):
+				if col is False:
+					score += int(self.lines[line_index][col_index])
+		return score
+
+	def print_board(self):
+		output = []
+		for line_index, line in enumerate(self.marked):
+			line_output = ""
+			for col_index, col in enumerate(line):
+				num = self.lines[line_index][col_index]
+				if col:
+					line_output += "[{}]".format(num)
+				else:
+					line_output += " {} ".format(num)
+			output.append(line_output)
+		for output_line in output:
+			print(output_line)
+
+def build_boards(lines):
+	result = []
+
+	board = []
+	for line in lines:
+		if len(board) < 5:
+			board.append(line)
+		else:
+			new_board = BingoBoard(board)
+			result.append(new_board)
+			board = []
+	return result
+
+
+def p1():
+	data = get_file_contents("data/day4_input.txt")
+	bingo_numbers = data.pop(0)
+	blank_line = data.pop(0)
+
+	boards = build_boards(data)
+
+	done = False
+
+	numbers_drawn = []
+
+	winning_board = None
+
+	for num in bingo_numbers.split(","):
+		numbers_drawn.append(num)
+		for board in boards:
+			board.check_number(num)
+			if board.check_board():
+				board_score = board.get_score()
+				score = board_score * int(num)
+				winning_board = board
+				break
+		else:
+			continue
+		break
+	print("Winner!")
+	winning_board.print_board()
+	print()
+
+	return score
+
+def p2():
+	pass
+
+
+if __name__ == '__main__':
+	# not 12924
+	print("Part 1:", p1())
+	print("Part 2:", p2())
