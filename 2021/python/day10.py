1ca796b5a16430de8d6030fd13fa1336d038eb96
Josh Bright <josh.bright@gmail.com>
2022-12-01 13:50:43 -0800

    Renamed day puzzles to include 2 digits to sort better.  Updated formatting to black

--- 2021/python/day10.py
+++ 2021/python/day10.py
@@ -6,41 +6,41 @@ closers = ["}", "]", ">", ")"]
 
 
 def check_line(line):
-	line_data = []
-	for c in line:
-		if c in openers:
-			# print("found opener", c)
-			line_data.append(c)
-		else:
-			# this character is a closing bracket
-			# we need to check if the last item in our line data list is the matching opener
-			# for this particular closer
+    line_data = []
+    for c in line:
+        if c in openers:
+            # print("found opener", c)
+            line_data.append(c)
+        else:
+            # this character is a closing bracket
+            # we need to check if the last item in our line data list is the matching opener
+            # for this particular closer
 
-			# get the index of the current character
-			closer_index = closers.index(c)
+            # get the index of the current character
+            closer_index = closers.index(c)
 
-			# this is the opener character for the current closing bracket
-			opener_char = openers[closer_index]
+            # this is the opener character for the current closing bracket
+            opener_char = openers[closer_index]
 
-			# check if the last item in our list is the matching opening statement
-			if opener_char == line_data[-1]:
-				# print("found closer", c, "matching opener", opener_char)
-				# if so, we can remove the item in our list
-				line_data.pop()
-			else:
-				# print("last opener", line_data[-1], "didn't match", opener_char)
-				#  if not, then this is an invalid string
-				return "corrupt", c
+            # check if the last item in our list is the matching opening statement
+            if opener_char == line_data[-1]:
+                # print("found closer", c, "matching opener", opener_char)
+                # if so, we can remove the item in our list
+                line_data.pop()
+            else:
+                # print("last opener", line_data[-1], "didn't match", opener_char)
+                #  if not, then this is an invalid string
+                return "corrupt", c
 
-	if len(line_data):
-		# print("line incomplete, we have", len(line_data), "items left")
-		# print(line_data)
-		return "incomplete", line_data
-	return "normal", None
+    if len(line_data):
+        # print("line incomplete, we have", len(line_data), "items left")
+        # print(line_data)
+        return "incomplete", line_data
+    return "normal", None
 
 
 def get_data(source="real"):
-	test_data = """
+    test_data = """
 		[({(<(())[]>[[{[]{<()<>>
 		[(()[<>])]({[<{<<[]>>(
 		{([(<{}[<>[]}>{[]{[(<()>
@@ -53,93 +53,83 @@ def get_data(source="real"):
 		<{([{{}}[<[[[<>{}]]]>[]]
 	"""
 
-	if source == "real":
-		return get_file_contents("data/day10_input.txt")
-	else:
-		result = []
-		for line in test_data.split("\n"):
-			if line:
-				result.append(line.strip())
-		return result
+    if source == "real":
+        return get_file_contents("data/day10_input.txt")
+    else:
+        result = []
+        for line in test_data.split("\n"):
+            if line:
+                result.append(line.strip())
+        return result
 
 
 def organize_data(lines):
-	result = {
-		"corrupt": [],
-		"incomplete": [],
-		"normal": [],
-	}
+    result = {
+        "corrupt": [],
+        "incomplete": [],
+        "normal": [],
+    }
 
-	for line_index, line in enumerate(lines):
-		line_result = check_line(line)
-		# print("----------------", line_index, line_result)
-		if line_result[0] in ["corrupt", "incomplete"]:
-			result[line_result[0]].append([line, line_result[1]])
-		else:	
-			result[line_result[0]].append(line)
-	return result
+    for line_index, line in enumerate(lines):
+        line_result = check_line(line)
+        # print("----------------", line_index, line_result)
+        if line_result[0] in ["corrupt", "incomplete"]:
+            result[line_result[0]].append([line, line_result[1]])
+        else:
+            result[line_result[0]].append(line)
+    return result
 
 
 def p1():
-	lines = get_data()
-	result = organize_data(lines)
+    lines = get_data()
+    result = organize_data(lines)
 
-	points = {
-		"}": 1197,
-		")": 3,
-		"]": 57,
-		">": 25137
-	}
+    points = {"}": 1197, ")": 3, "]": 57, ">": 25137}
 
-	score = 0
-	for corrupt_item in result["corrupt"]:
-		score += points[corrupt_item[1]]
-	return score
+    score = 0
+    for corrupt_item in result["corrupt"]:
+        score += points[corrupt_item[1]]
+    return score
 
 
 def fix_line(line_items):
-	result = []
-	for item in line_items:
-		opener_index = openers.index(item)
-		closer_char = closers[opener_index]
-		result.append(closer_char)
-	result.reverse()
-	return result
+    result = []
+    for item in line_items:
+        opener_index = openers.index(item)
+        closer_char = closers[opener_index]
+        result.append(closer_char)
+    result.reverse()
+    return result
 
 
 def score_item(char_list):
-	points = {
-		")": 1,
-		"]": 2,
-		"}": 3,
-		">": 4
-	}
+    points = {")": 1, "]": 2, "}": 3, ">": 4}
 
-	score = 0
+    score = 0
 
-	for item in char_list:
-		score *= 5
-		item_point = points[item]
-		score += item_point
-	return score
+    for item in char_list:
+        score *= 5
+        item_point = points[item]
+        score += item_point
+    return score
 
 
 def p2():
-	lines = get_data("real")
-	result = organize_data(lines)
+    lines = get_data("real")
+    result = organize_data(lines)
 
-	score_list = []
-	
-	for item in result["incomplete"]:
-		fix = fix_line(item[1])
-		score = score_item(fix)
-		score_list.append(score)
-	score_list = sorted(score_list)
-	len_scores = len(score_list)
-	middle_index = len_scores // 2
-	return score_list[middle_index]
+    score_list = []
+
+    for item in result["incomplete"]:
+        fix = fix_line(item[1])
+        score = score_item(fix)
+        score_list.append(score)
+    score_list = sorted(score_list)
+    len_scores = len(score_list)
+    middle_index = len_scores // 2
+    return score_list[middle_index]
 
 
-if __name__ == '__main__':
-	print("Part 1:", p1())
-	print("Part 2:", p2())
+if __name__ == "__main__":
+    print("Part 1:", p1())
+    print("Part 2:", p2())
--- 2021/python/day11.py
+++ 2021/python/day11.py
@@ -2,192 +2,187 @@ from common import get_file_contents
 
 
 def get_data(debug=False):
-	debug_data = """
-		5483143223
-		2745854711
-		5264556173
-		6141336146
-		6357385478
-		4167524645
-		2176841721
-		6882881134
-		4846848554
-		5283751526
-	"""
-	debug_data_mini = """
-		11111
-		19991
-		19191
-		19991
-		11111
-	"""
-	if debug:
-		result = []
-		for line in debug_data.split("\n"):
-			if len(line) > 1:
-				result.append(line.strip())
-		return result
-	else:
-		return get_file_contents("data/day11_data.txt")
+    debug_data = """
+        5483143223
+        2745854711
+        5264556173
+        6141336146
+        6357385478
+        4167524645
+        2176841721
+        6882881134
+        4846848554
+        5283751526
+    """
+    debug_data_mini = """
+        11111
+        19991
+        19191
+        19991
+        11111
+    """
+    if debug:
+        result = []
+        for line in debug_data.split("\n"):
+            if len(line) > 1:
+                result.append(line.strip())
+        return result
+    else:
+        return get_file_contents("data/day11_data.txt")
 
 
 def print_grid(grid):
-	for row in grid:
-		print(str(row))
-	print("----")
+    for row in grid:
+        print(str(row))
+    print("----")
 
 
 def add_energy_step(old_step):
-	new_step = []
-	for row_index, row in enumerate(old_step):
-		new_step.append([])
-		for col_index, col in enumerate(row):
-			new_step[row_index].append(int(col) + 1)
-	return new_step
+    new_step = []
+    for row_index, row in enumerate(old_step):
+        new_step.append([])
+        for col_index, col in enumerate(row):
+            new_step[row_index].append(int(col) + 1)
+    return new_step
 
 
 def lines_to_grid(lines):
-	result = []
-	for row in lines:
-		new_row = []
-		for char in list(row):
-			new_row.append(int(char))
-		result.append(new_row)
-	return result
+    result = []
+    for row in lines:
+        new_row = []
+        for char in list(row):
+            new_row.append(int(char))
+        result.append(new_row)
+    return result
 
 
 def get_neighbors(x, y, width, height):
-	result = []
-	if x > 0 and y > 0:
-		result.append([x - 1, y - 1]) # upper left
+    result = []
+    if x > 0 and y > 0:
+        result.append([x - 1, y - 1])  # upper left
 
-	if y > 0:
-		result.append([x, y - 1])     # top
+    if y > 0:
+        result.append([x, y - 1])  # top
 
-	if x < width and y > 0:
-		result.append([x + 1, y - 1]) # upper right
-	
-	if x > 0:
-		result.append([x - 1, y])     # left
-	
-	if x < width:
-		result.append([x + 1, y])     # right
-	
-	if x > 0 and y < height:
-		result.append([x - 1, y + 1]) # lower left
-	
-	if y < height:
-		result.append([x, y + 1])     # bottom
-	
-	if x < width and y < height:
-		result.append([x + 1, y + 1]) # lower right
-	return result
+    if x < width and y > 0:
+        result.append([x + 1, y - 1])  # upper right
+
+    if x > 0:
+        result.append([x - 1, y])  # left
+
+    if x < width:
+        result.append([x + 1, y])  # right
+
+    if x > 0 and y < height:
+        result.append([x - 1, y + 1])  # lower left
+
+    if y < height:
+        result.append([x, y + 1])  # bottom
+
+    if x < width and y < height:
+        result.append([x + 1, y + 1])  # lower right
+    return result
 
 
 def check_for_flashes(grid):
-	debug = True
-	blinks = 0
-	done = False
-	rounds = 0
-	total_items = len(grid) * len(grid[0])
-	print("Total Items:", total_items)
+    debug = True
+    blinks = 0
+    done = False
+    rounds = 0
+    total_items = len(grid) * len(grid[0])
+    print("Total Items:", total_items)
 
-	grid_width = len(grid[0])
-	grid_height = len(grid)
-	blinks_this_round = 0
+    grid_width = len(grid[0])
+    grid_height = len(grid)
+    blinks_this_round = 0
 
-	# during the process of flashing, we may introduce more flashes
-	# so we need to loop an unknown amount of times, until there
-	# are no more flashes
-	while not done:
-		rounds += 1
-		did_flash = False
-		
-		if debug:
-			print("Round", rounds)
-			print_grid(grid)
-		for row_index, row in enumerate(grid):
-			for col_index, col in enumerate(row):
-				if col > 9:
-					# blink
-					blinks += 1
-					blinks_this_round += 1
+    # during the process of flashing, we may introduce more flashes
+    # so we need to loop an unknown amount of times, until there
+    # are no more flashes
+    while not done:
+        rounds += 1
+        did_flash = False
 
-					# get list of neighbors
-					neighbor_list = get_neighbors(
-						col_index,
-						row_index,
-						grid_width,
-						grid_height
-					)
-					
-					for neighbor in neighbor_list:
-						try:
-							# if the neighbor has a 0
-							# then we should not do anything with that
-							# as its already blinked and been reset
-							nx, ny = neighbor
-							if grid[ny][nx] > 0:
-								grid[ny][nx] += 1
-								if grid[ny][nx] > 9:
-									pass
-						except IndexError:
-							pass
+        if debug:
+            print("Round", rounds)
+            print_grid(grid)
+        for row_index, row in enumerate(grid):
+            for col_index, col in enumerate(row):
+                if col > 9:
+                    # blink
+                    blinks += 1
+                    blinks_this_round += 1
 
-					# set energy level to 0
-					grid[row_index][col_index] = 0
+                    # get list of neighbors
+                    neighbor_list = get_neighbors(
+                        col_index, row_index, grid_width, grid_height
+                    )
 
-					# mark that we did flash at least something on this round
-					did_flash = True
-		if debug:
-			print("After Round")
-			print_grid(grid)
-		print("Blinks this round:", blinks_this_round)
-		if blinks_this_round == total_items:
-			print("All items flashed!!!!!!!!!!!!!!!!")
-			return None, None
-		if blinks_this_round == 99:
-			print_grid(grid)
-		if not did_flash:
-			# if we didn't flash, we can stop
-			done = True
-	print("Rounds", rounds)
-	return blinks, grid
-					
+                    for neighbor in neighbor_list:
+                        try:
+                            # if the neighbor has a 0
+                            # then we should not do anything with that
+                            # as its already blinked and been reset
+                            nx, ny = neighbor
+                            if grid[ny][nx] > 0:
+                                grid[ny][nx] += 1
+                                if grid[ny][nx] > 9:
+                                    pass
+                        except IndexError:
+                            pass
+
+                    # set energy level to 0
+                    grid[row_index][col_index] = 0
+
+                    # mark that we did flash at least something on this round
+                    did_flash = True
+        if debug:
+            print("After Round")
+            print_grid(grid)
+        print("Blinks this round:", blinks_this_round)
+        if blinks_this_round == total_items:
+            print("All items flashed!!!!!!!!!!!!!!!!")
+            return None, None
+        if blinks_this_round == 99:
+            print_grid(grid)
+        if not did_flash:
+            # if we didn't flash, we can stop
+            done = True
+    print("Rounds", rounds)
+    return blinks, grid
 
 
 def perform_steps(amount, grid):
-	flash_count = 0
-	# print("Before any steps")
-	# print_grid(grid)
-	for i in range(amount):
-		print("Step", str(i + 1))
-		grid = add_energy_step(grid)
-		# print("After Energy Step")
-		# print_grid(grid)
-		flashes, grid = check_for_flashes(grid)
-		if flashes:
-			flash_count += flashes
-		elif flashes is None and grid is None:
-			break
-		# print("Flashes:", flashes)
-		# print("Total Flashes:", flash_count)
-		# print_grid(grid)
-
+    flash_count = 0
+    # print("Before any steps")
+    # print_grid(grid)
+    for i in range(amount):
+        print("Step", str(i + 1))
+        grid = add_energy_step(grid)
+        # print("After Energy Step")
+        # print_grid(grid)
+        flashes, grid = check_for_flashes(grid)
+        if flashes:
+            flash_count += flashes
+        elif flashes is None and grid is None:
+            break
+        # print("Flashes:", flashes)
+        # print("Total Flashes:", flash_count)
+        # print_grid(grid)
 
 
 def p1():
-	lines = get_data()
-	grid = lines_to_grid(lines)
-	perform_steps(100, grid)
+    lines = get_data()
+    grid = lines_to_grid(lines)
+    perform_steps(100, grid)
 
 
 def p2():
-	lines = get_data()
-	grid = lines_to_grid(lines)
-	perform_steps(500, grid)
+    lines = get_data()
+    grid = lines_to_grid(lines)
+    perform_steps(500, grid)
 
 
-if __name__ == '__main__':
-	# print("Part 1:", p1())
-	print("Part 2:", p2())
+if __name__ == "__main__":
+    # print("Part 1:", p1())
+    print("Part 2:", p2())
