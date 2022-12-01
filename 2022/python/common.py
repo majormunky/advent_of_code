dfd11dcbe4663ec6b8ff5337040b23da05c985ea
Josh Bright <josh.bright@gmail.com>
2022-12-01 09:10:06 -0800

    2022 Day 1 Complete

---
+++ 2022/python/common.py
@@ -0,0 +1,14 @@
+import os
+
+
+DATA_DIR = os.path.join(".", "data")
+
+
+def get_file_contents(filepath, single_line=False):
+    lines = []
+    with open(filepath, "r") as f:
+        for line in f.readlines():
+            lines.append(line.strip())
+    if single_line:
+        return lines[0]
+    return lines
---
+++ 2022/python/day1.py
@@ -0,0 +1,52 @@
+import os
+import common
+
+
+def get_calorie_count(items):
+    return sum(items)
+
+
+def get_data():
+    filepath = os.path.join(common.DATA_DIR, "day1_input.txt")
+    lines = common.get_file_contents(filepath)
+
+    elves = []
+
+    new_group = []
+    for line in lines:
+        if line == "":
+            elves.append(new_group)
+            new_group = []
+        else:
+            new_group.append(int(line))
+    return elves
+
+
+def p1():
+    elves = get_data()
+
+    most_calories = 0
+
+    for elf in elves:
+        count = get_calorie_count(elf)
+        if count > most_calories:
+            most_calories = count
+
+    print(most_calories)
+
+
+def p2():
+    elves = get_data()
+    counts = []
+
+    for elf in elves:
+        counts.append(get_calorie_count(elf))
+
+    counts = sorted(counts)
+
+    total = counts[-1] + counts[-2] + counts[-3]
+    print(total)
+
+
+if __name__ == "__main__":
+    p2()
