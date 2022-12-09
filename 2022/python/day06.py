4f3582f856767d03fc78b1a45a1f44682222997e
Josh Bright <josh.bright@gmail.com>
2022-12-08 18:01:42 -0800

    2022 Day 6 Part 2 Completed

--- 2022/python/day06.py
+++ 2022/python/day06.py
@@ -1,19 +1,20 @@
 from common import get_file_contents
 
 
-def process_data(letter_list):
+def process_data(letter_list, size):
     temp_data = []
 
     for index, letter in enumerate(letter_list):
-        if len(temp_data) < 4:
+        if len(temp_data) < size:
             temp_data.append(letter)
             continue
 
-        if len(set(temp_data)) == 4:
+        if len(set(temp_data)) == size:
             return index
 
         temp_data.append(letter)
         temp_data.pop(0)
+
 
 def test_p1():
     test_letters = [
@@ -23,9 +24,9 @@ def test_p1():
         (list("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10),
         (list("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11)
     ]
-
+    chunk_size = 4
     for test_list, expected_val in test_letters:
-        result = process_data(test_list)
+        result = process_data(test_list, chunk_size)
         assert result == expected_val
 
     print("Finished Tests")
@@ -34,16 +35,21 @@ def test_p1():
 def p1():
     lines = get_file_contents("data/day06_input.txt")
     letters = list(lines[0])
-
-    result = process_data(letters)
+    chunk_size = 4
+    result = process_data(letters, chunk_size)
     print(result)
 
+
 def p2():
-    pass
+    lines = get_file_contents("data/day06_input.txt")
+    letters = list(lines[0])
+    chunk_size = 14
+    result = process_data(letters, chunk_size)
+    print(result)
 
 
 def main():
-    p1()
+    p2()
 
 
 if __name__ == "__main__":
