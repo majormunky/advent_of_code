4091dd88e597f6d3493cb37dd0703dcac22991c0
Josh Bright <josh.bright@gmail.com>
2023-02-14 21:01:18 -0800

    2018 Day 1 Completed (Python)

--- 2018/day1.py
+++ 2018/day1.py
@@ -1,5 +1,6 @@
 import sys
 import common
+import itertools
 
 
 def get_filename():
@@ -7,6 +8,7 @@ def get_filename():
     filename = filename.split("/")[-1]
     filename = filename.split(".")[0]
     return filename
+
 
 data = common.get_file_contents("data/{}_input.txt".format(get_filename()))
 
@@ -22,7 +24,23 @@ def part1():
 
 
 def part2():
-    return "not complete"
+    frequency = 0
+    seen_frequencies = []
+
+    for row in itertools.cycle(data):
+        freq_number = int(row[1:])
+
+        if "-" in row:
+            frequency -= freq_number
+        else:
+            frequency += freq_number
+
+        if frequency in seen_frequencies:
+            return frequency
+        else:
+            seen_frequencies.append(frequency)
+
+    print(len(seen_frequencies))
 
 
 def main():
@@ -33,6 +51,5 @@ def main():
     print(f"Part 2: {part2_answer}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
