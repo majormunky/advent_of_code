131353e30157ce700ad8e3eb398578aa6f82cdf6
Josh Bright <josh.bright@gmail.com>
2023-12-11 21:18:12 -0800

    2023 Day 8 Part 2 Completed (Python)

--- 2023/python/day08.py
+++ 2023/python/day08.py
@@ -1,14 +1,31 @@
+import math
+import sys
+import time
+import json
+
 test_lines = ["LLR", "", "AAA = (BBB, BBB)", "BBB = (AAA, ZZZ)", "ZZZ = (ZZZ, ZZZ)"]
+test_lines_2 = [
+    "LR",
+    "",
+    "11A = (11B, XXX)",
+    "11B = (XXX, 11Z)",
+    "11Z = (11B, XXX)",
+    "22A = (22B, XXX)",
+    "22B = (22C, 22C)",
+    "22C = (22Z, 22Z)",
+    "22Z = (22B, 22B)",
+    "XXX = (XXX, XXX)",
+]
+
 
 import common
 
 
-def get_data(debug):
+def get_data(debug, test_data):
     if debug:
-        data = test_lines
+        return test_data
     else:
-        data = common.get_file_contents("data/day08_input.txt")
-    return data
+        return common.get_file_contents("data/day08_input.txt")
 
 
 def parse_lines(lines):
@@ -24,7 +41,7 @@ def parse_lines(lines):
 
 
 def part1(debug=True):
-    lines = get_data(debug)
+    lines = get_data(debug, test_lines)
 
     data = parse_lines(lines)
 
@@ -44,10 +61,41 @@ def part1(debug=True):
     print(steps)
 
 
+def find_steps(key, directions, paths):
+    steps = 0
+
+    current_value = key
+    while True:
+        for direction in directions:
+            steps += 1
+            current_value = paths[current_value][direction]
+            if current_value.endswith("Z"):
+                return steps
+
+
 def part2(debug=True):
-    lines = get_data(debug)
+    lines = get_data(debug, test_lines_2)
+    puzzle_data = parse_lines(lines)
+
+    directions = puzzle_data["directions"]
+    paths = puzzle_data["paths"]
+
+    result = {}
+
+    nodes = {}
+
+    for key in puzzle_data["paths"].keys():
+        if key.endswith("A"):
+            nodes[key] = key
+
+    for k, v in nodes.items():
+        result[k] = find_steps(k, directions, paths)
+
+    step_list = result.values()
+
+    print(math.lcm(*step_list))
 
 
 if __name__ == "__main__":
     part1(False)
-    part2()
+    part2(False)
