cd941175ff392738f077360794dc39289a3b53f0
Josh Bright <josh.bright@gmail.com>
2024-01-12 14:02:49 -0800

    2022 Day 11 Part 2 Completed (Python)

--- 2022/python/day11.py
+++ 2022/python/day11.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import math
 from common import get_file_contents
 
 
@@ -18,8 +19,9 @@ def get_chunks(lines):
 
 
 class Monkey:
-    def __init__(self, lines):
+    def __init__(self, lines, trainer):
         self.name = self.build_name(lines[0])
+        self.trainer = trainer
         self.item_list = self.get_starting_item_list(lines[1])
         self.operation_val = None
         self.operation_type = None
@@ -85,13 +87,38 @@ class Monkey:
 
         return new_func
 
+    def process_fast(self, modulo_number):
+        result = []
+        current_items = list(self.item_list)
+        for item in current_items:
+            self.inspection_count += 1
+
+            worry_level = self.perform_operation(item)
+            worry_level = worry_level % modulo_number
+
+            test_val = self.item_test(worry_level)
+
+            test_result = {"monkey": None, "item": None}
+            if test_val:
+                test_result["monkey"] = self.next_monkey["passes"]
+                test_result["item"] = worry_level
+            else:
+                test_result["monkey"] = self.next_monkey["fails"]
+                test_result["item"] = worry_level
+
+            result.append(test_result)
+            self.item_list.remove(item)
+        return result
+
     def process(self):
         result = []
         current_items = list(self.item_list)
         for item in current_items:
             self.inspection_count += 1
             worry_level = self.perform_operation(item)
-            worry_level = worry_level // 3
+
+            if self.trainer.perform_worry_reduction:
+                worry_level = worry_level // 3
 
             test_val = self.item_test(worry_level)
 
@@ -111,19 +138,37 @@ class Monkey:
 
 
 class MonkeyTrainer:
-    def __init__(self, chunks):
+    def __init__(self, chunks, perform_worry_reduction):
         self.monkeys = {}
+        self.perform_worry_reduction = perform_worry_reduction
         self.build_monkeys(chunks)
+        self.all_monkey_modulo = self.calculate_monkey_modulo()
+        print("Monkey Modulo", self.all_monkey_modulo)
 
     def build_monkeys(self, chunk_list):
         for chunk in chunk_list:
-            monkey = Monkey(chunk)
+            monkey = Monkey(chunk, self)
             self.monkeys[monkey.name] = monkey
         print(f"Built {len(self.monkeys)} monkeys")
+
+    def calculate_monkey_modulo(self):
+        result = 0
+        items = []
+
+        for _, monkey in self.monkeys.items():
+            items.append(monkey.item_test_val)
+        return math.prod(items)
 
     def perform_round(self):
         for monkey_name, monkey in self.monkeys.items():
             results = monkey.process()
+
+            for result in results:
+                self.monkeys[result["monkey"]].catch_item(result["item"])
+
+    def perform_round_fast(self):
+        for monkey_name, monkey in self.monkeys.items():
+            results = monkey.process_fast(self.all_monkey_modulo)
 
             for result in results:
                 self.monkeys[result["monkey"]].catch_item(result["item"])
@@ -137,7 +182,10 @@ class MonkeyTrainer:
 
     def output_monkey_inspection_counts(self):
         for monkey_name, monkey in self.monkeys.items():
-            print(f"{monkey_name}: {monkey.inspection_count}")
+            print(
+                f"Monkey {monkey_name} inspected items {monkey.inspection_count} times"
+            )
+        print()
 
     def get_monkey_business_amount(self):
         values = []
@@ -146,22 +194,42 @@ class MonkeyTrainer:
         values.sort()
         return values[-1] * values[-2]
 
-    def run(self, rounds):
+    def run(self, rounds, fast_version=False):
+        round_count = 0
         for i in range(rounds):
-            self.perform_round()
+            if fast_version:
+                self.perform_round_fast()
+            else:
+                self.perform_round()
+            round_count += 1
+
+            if round_count == 1:
+                print(f"== After round {round_count} == ")
+                self.output_monkey_inspection_counts()
+            elif round_count == 20:
+                print(f"== After round {round_count} == ")
+                self.output_monkey_inspection_counts()
+            elif round_count % 1000 == 0:
+                print(f"== After round {round_count} == ")
+                self.output_monkey_inspection_counts()
 
 
 def part1():
     lines = get_file_contents("data/day11_input.txt")
     chunks = get_chunks(lines)
-    trainer = MonkeyTrainer(chunks)
+    trainer = MonkeyTrainer(chunks, True)
     trainer.run(20)
     print(trainer.get_monkey_business_amount())
 
 
 def part2():
-    pass
+    lines = get_file_contents("data/day11_input.txt")
+    chunks = get_chunks(lines)
+    trainer = MonkeyTrainer(chunks, False)
+    trainer.run(10000, True)
+    print(trainer.get_monkey_business_amount())
 
 
 if __name__ == "__main__":
-    part1()  # 110888
+    # part1()  # 110888
+    part2()  # 25590400731
