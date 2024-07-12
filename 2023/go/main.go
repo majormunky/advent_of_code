13f2d05d888dc756026edc53623bc8517153a219
Josh Bright <josh.bright@gmail.com>
2024-07-12 13:12:29 -0700

    Updated main go file to dynamically call the days function passed in as an argument

--- 2023/go/main.go
+++ 2023/go/main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"reflect"
 
 	"joshbright.dev/aoc/days"
 )
 
-func main() {
-	day01_part01 := days.Day01Part1()
-	day01_part02 := days.Day01Part2()
+func runFunction(functionName string) {
+	funcMap := map[string]interface{}{
+		"Day01Part1": days.Day01Part1,
+		"Day01Part2": days.Day01Part2,
+	}
 
-	fmt.Printf("Day 1 Part 1: %v\n", day01_part01)
-	fmt.Printf("Day 1 Part 2: %v\n", day01_part02)
+	fn := reflect.ValueOf(funcMap[functionName])
+	if fn.IsValid() {
+		result := fn.Call(nil)
+		fmt.Printf("%v: %v\n", functionName, result[0])
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+
+	funcName1 := fmt.Sprintf("%vPart1", args[0])
+	funcName2 := fmt.Sprintf("%vPart2", args[0])
+
+	runFunction(funcName1)
+	runFunction(funcName2)
 }
