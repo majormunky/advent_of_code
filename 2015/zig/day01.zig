082c8f26eaa8f340bb79a515739e8e173f2305cf
Josh Bright <josh.bright@gmail.com>
2024-10-04 22:59:23 -0700

    2015 Day 1 Part 2 Zig completed

--- 2015/zig/day01.zig
+++ 2015/zig/day01.zig
@@ -11,8 +11,10 @@ fn get_file_contents(allocator: std.mem.Allocator, path: []const u8) ![]u8 {
 }
 
 pub fn main() !void {
-    const answer_day01 = try part01();
-    std.debug.print("Day01: {d}\n", .{answer_day01});
+    const answer_part01 = try part01();
+    const answer_part02 = try part02();
+    std.debug.print("Part 1: {d}\n", .{answer_part01});
+    std.debug.print("Part 2: {d}\n", .{answer_part02});
 }
 
 fn part01() !i64 {
@@ -31,4 +33,27 @@ fn part01() !i64 {
     }
 
     return floor;
+}
+
+fn part02() !usize {
+    var allocator = std.heap.page_allocator;
+    const contents = try get_file_contents(allocator, "../data/day01_input.txt");
+    defer allocator.free(contents);
+
+    var floor: i64 = 0;
+    var result: usize = 0;
+
+    for (contents, 1..) |character, index| {
+        if (character == ')') {
+            floor -= 1;
+        } else if (character == '(') {
+            floor += 1;
+        }
+        if (floor < 0) {
+            result = index;
+            break;
+        }
+    }
+
+    return result;
 }
