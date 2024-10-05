eac163db009c664b169c27e4aca824b3392f8de9
Josh Bright <josh.bright@gmail.com>
2024-10-05 16:09:28 -0700

    Updated zig function to read file to size buffer using file size instead of a hard coded value

--- 2015/zig/utils.zig
+++ 2015/zig/utils.zig
@@ -4,8 +4,11 @@ pub fn get_file_contents(allocator: std.mem.Allocator, path: []const u8) ![]u8 {
     // Open the file for reading
     const file = try std.fs.cwd().openFile(path, .{});
 
+    // Get stats of file
+    const fileStat = try file.stat();
+
     // Read the contents of the file into a buffer
-    const contents = try file.readToEndAlloc(allocator, 20000);
+    const contents = try file.readToEndAlloc(allocator, fileStat.size);
     defer file.close();
     return contents;
 }
