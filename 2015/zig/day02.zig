847d7aa63f71be8ff339a2caf67749fd9f048b67
Josh Bright <josh.bright@gmail.com>
2024-10-05 23:06:37 -0700

    2015 Day 2 Part 2 Zig Completed

--- 2015/zig/day02.zig
+++ 2015/zig/day02.zig
@@ -27,6 +27,18 @@ const Box = struct {
         return self.height * self.length;
     }
 
+    pub fn get_length_perimeter(self: Box) i32 {
+        return self.length + self.length + self.width + self.width;
+    }
+
+    pub fn get_width_perimeter(self: Box) i32 {
+        return self.width + self.width + self.height + self.height;
+    }
+
+    pub fn get_height_perimeter(self: Box) i32 {
+        return self.height + self.height + self.length + self.length;
+    }
+
     pub fn get_size(self: Box) i32 {
         const length = self.get_length();
         const width = self.get_width();
@@ -42,10 +54,27 @@ const Box = struct {
 
         return (length * 2) + (width * 2) + (height * 2) + smallest;
     }
+
+    pub fn get_ribbon_amount(self: Box) i32 {
+        const length = self.get_length_perimeter();
+        const width = self.get_width_perimeter();
+        const height = self.get_height_perimeter();
+
+        var smallest = length;
+        if (width < smallest) {
+            smallest = width;
+        }
+        if (height < smallest) {
+            smallest = height;
+        }
+
+        return smallest + self.length * self.width * self.height;
+    }
 };
 
 pub fn main() !void {
     try part01();
+    try part02();
 }
 
 fn part01() !void {
@@ -64,9 +93,32 @@ fn part01() !void {
     std.debug.print("Answer: {d}\n", .{answer});
 }
 
+fn part02() !void {
+    var allocator = std.heap.page_allocator;
+    const contents = try utils.get_file_contents(allocator, "../data/day02_input.txt");
+    defer allocator.free(contents);
+
+    var answer: i64 = 0;
+
+    var iter = std.mem.tokenizeSequence(u8, contents, "\n");
+    while (iter.next()) |line| {
+        var box = try Box.init(line);
+        answer += box.get_ribbon_amount();
+    }
+
+    std.debug.print("Answer: {d}\n", .{answer});
+}
+
 test "box calculates size correctly" {
     const testLine: []const u8 = "2x3x4";
     const box = try Box.init(testLine);
     const boxSize = box.get_size();
     try std.testing.expect(boxSize == 58);
+}
+
+test "box calculates ribbon amount correctly" {
+    const testLine: []const u8 = "2x3x4";
+    const box = try Box.init(testLine);
+    const boxSize = box.get_ribbon_amount();
+    try std.testing.expect(boxSize == 34);
 }
