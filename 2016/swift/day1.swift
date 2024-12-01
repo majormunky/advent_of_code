29dc36f19b4dff1908c90e222f3c8031c82f6c64
Josh Bright <josh.bright@gmail.com>
2024-12-01 10:36:08 -0800

    Cleanup of swift file

--- 2016/swift/day1.swift
+++ 2016/swift/day1.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-let puzzle_input = "L5, R1, R4, L5, L4, R3, R1, L1, R4, R5, L1, L3, R4, L2, L4, R2, L4, L1, R3, R1, R1, L1, R1, L5, R5, R2, L5, R2, R1, L2, L4, L4, R191, R2, R5, R1, L1, L2, R5, L2, L3, R4, L1, L1, R1, R50, L1, R1, R76, R5, R4, R2, L5, L3, L5, R2, R1, L1, R2, L3, R4, R2, L1, L1, R4, L1, L1, R185, R1, L5, L4, L5, L3, R2, R3, R1, L5, R1, L3, L2, L2, R5, L1, L1, L3, R1, R4, L2, L1, L1, L3, L4, R5, L2, R3, R5, R1, L4, R5, L3, R3, R3, R1, R1, R5, R2, L2, R5, L5, L4, R4, R3, R5, R1, L3, R1, L2, L2, R3, R4, L1, R4, L1, R4, R3, L1, L4, L1, L5, L2, R2, L1, R1, L5, L3, R4, L1, R5, L5, L5, L1, L3, R1, R5, L2, L4, L5, L1, L1, L2, R5, R5, L4, R3, L2, L1, L3, L4, L5, L5, L2, R4, R3, L5, R4, R2, R1, L5"
-
 let test_puzzle_input = "R2, L3"
 
 func get_distance(x: Int, y: Int) -> Int {
@@ -19,7 +17,7 @@ func get_new_direction(current: String, turn_direction: String) -> String {
     let result: String
     let directions = ["N", "W", "S", "E"]
     let dir_index = directions.firstIndex(of: current)!
-    
+
     if turn_direction == "L" {
         if dir_index == 0 {
             result = directions[3]
@@ -33,7 +31,7 @@ func get_new_direction(current: String, turn_direction: String) -> String {
             result = directions[dir_index + 1]
         }
     }
-    
+
     return result
 }
 
@@ -45,10 +43,9 @@ func part1() -> Int {
         var action = item.trimmingCharacters(in: .whitespaces)
         let direction = String(action.removeFirst())
         let steps = Int(action)!
-        
+
         let new_direction = get_new_direction(current: current_direction, turn_direction: direction)
-        
-        
+
         switch new_direction {
         case "N":
             y_pos += steps
@@ -61,10 +58,10 @@ func part1() -> Int {
         default:
             print("Unknown new direction: \(new_direction)")
         }
-        
+
         current_direction = String(new_direction)
     }
-    
+
     let result = get_distance(x: x_pos, y: y_pos)
     return result
 }
